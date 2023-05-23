5d3dcdda8ca9ee0b89cb5cf486a3f9a1d2ac3c6b
schuler-henry <72646334+schuler-henry@users.noreply.github.com>
2023-05-23 22:41:39 +0200

    Updated and completed CLI parsing for the client. Added logger to project.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <sstream>
-#include <spdlog/spdlog.h>
 #include <list>
 
+#include "../include/spdlog/spdlog.h"
 #include "client.h"
 
 using namespace std;
 
+void printUsageInformation();
 bool isValidIPv4Address(const string &ipAddress);
 int getPortNumber(const string &portStr);
 
@@ -26,9 +27,10 @@ int main(int argc, char** argv) {
     spdlog::info("Parsing CLI arguments...");
 
     int port = 0;
-    string serverAddress = "";
-    int serverPort = -1;
-    list<list<string>> commands = {};
+    string serverAddress = "127.0.0.1";
+    int serverPort = 8080;
+    string command = "";
+    list<string> arguments = {};
 
     // Parse CLI arguments and validate their values
     for (int i = 1; i < argc; i++) {
@@ -74,53 +76,59 @@ int main(int argc, char** argv) {
             spdlog::info("Server-Port: {}", port);
 
             i += 1;
-        } else if (arg == "--subscribe" && argc > i + 1) {
-            string topicName = argv[i + 1];
+        } else if (arg == "--help") {
+            // Print help message and terminate
+            spdlog::info("Printing help information...");
+            printUsageInformation();
+            spdlog::info("Shutting down...");
+            return 0;
+        }
+        else
+        {
+            // Argument is a command or an argument for a command is missing
+            // One client can only perform one kind of command therefore the first command defines the command type
+            if (command == "") {
+                command = arg;
+            }
 
-            list<string> command = {"subscribe", topicName};
-            commands.push_back(command);
+            if (arg == "--subscribe" && arg == command && argc > i + 1) {
+                while (argc > i + 1 && argv[i + 1][0] != '-' && argv[i + 1][1] != '-') {
+                    string topicName = argv[i + 1];
 
-            spdlog::info("-> Subscribing to topic: {}", topicName);
+                    arguments.push_back(topicName);
+                
+                    spdlog::info("-> Subscribing to topic: {}", topicName);
 
-            i += 1;
-        } else if (arg == "--unsubscribe" && argc > i + 1) {
-            string topicName = argv[i + 1];
+                    i += 1;
+                }
+            } else if (arg == "--publish" && arguments.size() == 0 && argc > i + 2) {
+                string topicName = argv[i + 1];
+                string topicData = argv[i + 2];
 
-            list<string> command = {"unsubscribe", topicName};
-            commands.push_back(command);
+                arguments.push_back(topicName);
+                arguments.push_back(topicData);
 
-            spdlog::info("-> Unsubscribing from topic: {}", topicName);
+                spdlog::info("-> Publishing data: {} to topic: {}", topicData, topicName);
 
-            i += 1;
-        } else if (arg == "--publish" && argc > i + 2) {
-            string topicName = argv[i + 1];
-            string topicData = argv[i + 2];
+                i += 2;
+            } else if (arg == "--list-topics" && arg == command) {
+                spdlog::info("-> List topics");
+            } else if (arg == "--get-topic-status" && arg == command && argc > i + 1) {
+                while (argc > i + 1 && argv[i + 1][0] != '-' && argv[i + 1][1] != '-') {
+                    string topicName = argv[i + 1];
 
-            list<string> command = {"publish", topicName, topicData};
-            commands.push_back(command);
+                    arguments.push_back(topicName);
 
-            spdlog::info("-> Publishing data: {} to topic: {}", topicData, topicName);
+                    spdlog::info("-> Get topic status for topic: {}", topicName);
 
-            i += 2;
-        } else if (arg == "--list-topics") {
-            list<string> command = {"list-topics"};
-            commands.push_back(command);
-
-            spdlog::info("-> List topics");
-        } else if (arg == "--get-topic-status" && argc > i + 1) {
-            string topicName = argv[i + 1];
-
-            list<string> command = {"get-topic-status", topicName};
-            commands.push_back(command);
-
-            spdlog::info("-> Get topic status for topic: {}", topicName);
-
-            i += 1;
-        } else {
-            // Unknown CLI argument or missing values
-            spdlog::error("Unknown argument or missing values for: {}", arg);
-            spdlog::error("Shutting down...");
-            return 1;
+                    i += 1;
+                }
+            } else {
+                // Unknown CLI argument or missing values
+                spdlog::error("Unknown argument, invalid combination of commands or missing values for: {}", arg);
+                spdlog::error("Shutting down...");
+                return 1;
+            }
         }
     }
 
@@ -128,12 +136,66 @@ int main(int argc, char** argv) {
     spdlog::info("Starting client...");
 
     Client* client;
+
     try {
         client = new Client(port, serverAddress, serverPort);
-    } catch (const exception &e) {
+    } catch (const runtime_error &e) {
+        spdlog::error("{}", e.what());
+        spdlog::error("Shutting down...");
+        return 1;
     }
 
+    spdlog::info("Successfully started client.");
+    spdlog::info("Executing {}...", command);
+
+    if (command == "--subscribe") {
+        for (string topicName : arguments) {
+            client->subscribeTopic(topicName);
+        }
+    } else if (command == "--publish") {
+        client->publishTopic(arguments.front(), arguments.back());
+    } else if (command == "--list-topics") {
+        client->listTopics();
+    } else if (command == "--get-topic-status") {
+        for (string topicName : arguments) {
+            client->getTopicStatus(topicName);
+        }
+    } else {
+        spdlog::error("No command provided or unknown: \"{}\"", command);
+        spdlog::error("Shutting down...");
+        return 1;
+    }
+
+    spdlog::info("Shutting down...");
+    delete client;
+
     return 0;
+}
+
+void printUsageInformation() {
+    cout << endl << "Commandline Tool to start the subscriber client" << endl;
+    cout << endl << "Options:" << endl;
+    cout << "    " <<"--help: " << endl;
+    cout << "    " <<"    " <<"Print this help message" << endl;
+    cout << "    " <<"--port <PORT>: " << endl;
+    cout << "    " <<"    " <<"Port for connections to this client" << endl;
+    cout << "    " <<"    " <<"Default: 0 (random available port)" << endl;
+    cout << "    " <<"--server-address <ADDRESS>: " << endl;
+    cout << "    " <<"    " <<"IPv4 address of the publisher server" << endl;
+    cout << "    " <<"    " <<"Default: 127.0.0.1" << endl;
+    cout << "    " <<"--server-port <PORT>: " << endl;
+    cout << "    " <<"    " <<"Listening port of the publisher server" << endl;
+    cout << "    " <<"    " <<"Default: 8080" << endl;
+    cout << endl << "Commands (only one per client):" << endl;
+    cout << "    " <<"--subscribe <TOPIC1> <...> <TOPICn>: " << endl;
+    cout << "    " <<"    " <<"Subscribe to (multiple) topics" << endl;
+    cout << "    " <<"--publish <TOPIC> <MESSAGE>: " << endl;
+    cout << "    " <<"    " <<"Publish a message to a topic" << endl;
+    cout << "    " <<"--list-topics: " << endl;
+    cout << "    " <<"    " <<"List all existing topics" << endl;
+    cout << "    " <<"--get-topic-status <TOPIC1> <...> <TOPICn>: " << endl;
+    cout << "    " <<"    " <<"Get the status of (multiple) topics" << endl;
+    cout << endl;
 }
 
 int getPortNumber(const string& portStr) {
@@ -146,7 +208,7 @@ int getPortNumber(const string& portStr) {
     }
 
     // Port numbers are available from 0 to 65535
-    if (port > 65535) {
+    if (port > 65535 || port < 0) {
         return -1;
     }
 
