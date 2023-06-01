268c36e3f7e8b6f9fb8b3bb564b1a4a4c68c932d
schuler-henry <72646334+schuler-henry@users.noreply.github.com>
2023-06-01 16:32:13 +0200

    Initial files.

---
+++ src/shared/CommandIdentifiers.h
@@ -0,0 +1,10 @@
+/**
+ * @brief Specifies a Enum that describes the different Commands that can be sent from the client to the server.
+ */
+enum CommandIdentifiers{
+  SUBSCRIBE,
+  UNSUBSCRIBE,
+  PUBLISH,
+  LIST_TOPICS,
+  GET_TOPIC_STATUS,
+};
