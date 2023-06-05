d91d29cbfedee9d9d335b3a4056d5c13cd482268
Lukas Braun <lb.0108@web.de>
2023-06-05 23:19:33 +0200

    added compile instructions for include

--- src/shared/commandIdentifiers.h
+++ src/shared/commandIdentifiers.h
@@ -1,3 +1,5 @@
+#ifndef COMMANDIDENTIFIERS_H
+#define COMMANDIDENTIFIERS_H
 /**
  * @brief Specifies a Enum that describes the different Commands that can be sent from the client to the server.
  */
@@ -8,3 +10,4 @@ enum CommandIdentifiers{
   LIST_TOPICS,
   GET_TOPIC_STATUS,
 };
+#endif // COMMANDIDENTIFIERS_H
--- src/shared/statuscode.h
+++ src/shared/statuscode.h
@@ -1,6 +1,9 @@
+#ifndef STATUSCODE_H
+#define STATUSCODE_H
 enum Statuscode {
     success,
     failed,
     invalidParameter,
     internalError
 };
+#endif // STATUSCODE_H
